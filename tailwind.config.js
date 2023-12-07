48f2e72c413c83b287701a327e9d2b4e2a1e3bbf
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-07 12:36:29 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './js/app.js'],
+  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
     extend: {},
   },
   plugins: [],
