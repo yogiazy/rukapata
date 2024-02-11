62dd8e24a0c51d4ede97dd6d2203be56fee25d84
Yogi Prakoso <yogiarrazy@gmail.com>
2024-02-11 22:12:49 +0700

    update

---
+++ assets/js/mybook.js
@@ -0,0 +1,65 @@
+async function getBook() {
+    try {
+        const response = await fetch(`./data/buku.json`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch component: ${response.status}`);
+        }
+        const d = await response.text();
+        const y = JSON.parse(d).buku;
+        console.log(y[0]);
+
+        for (i = 0; i < y.length; i++) {
+            // Create parent container
+            const parentDiv = document.createElement('div');
+            parentDiv.id = `buku-${y[i].id}`;
+            parentDiv.classList.add('col-lg-4', 'col-md-6', 'grid-item', `${y[i].jenis}`);
+
+            // Create portfolio item wrapper
+            const portfolioItemWrapper = document.createElement('div');
+            portfolioItemWrapper.classList.add('portfolio-item-wrapper');
+
+            // Create portfolio image
+            const portfolioImg = document.createElement('div');
+            portfolioImg.classList.add('portfolio-img');
+            const img = document.createElement('img');
+            img.src = y[i].img;
+            img.alt = '';
+            portfolioImg.appendChild(img);
+
+            // Create portfolio overlay
+            const portfolioOverlay = document.createElement('div');
+            portfolioOverlay.classList.add('portfolio-overlay');
+
+            // Create overlay content
+            const overlayContent = document.createElement('div');
+            overlayContent.classList.add('overlay-content');
+            const span = document.createElement('span');
+            span.textContent = y[i].judul;
+            const h4 = document.createElement('h4');
+            h4.textContent = y[i].harga.softcover;
+            overlayContent.appendChild(span);
+            overlayContent.appendChild(h4);
+
+            // Append overlay content to overlay
+            portfolioOverlay.appendChild(overlayContent);
+
+            // Append portfolio image and overlay to portfolio item wrapper
+            portfolioItemWrapper.appendChild(portfolioImg);
+            portfolioItemWrapper.appendChild(portfolioOverlay);
+
+            // Append portfolio item wrapper to parent container
+            parentDiv.appendChild(portfolioItemWrapper);
+
+            // Find the element with ID "bukuRupakata"
+            const bukuRupakataElement = document.getElementById("bukuRupakata");
+
+            // Append the parent container to the "bukuRupakata" element
+            bukuRupakataElement.appendChild(parentDiv);
+
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getBook();
