e4eaa21bf1e7ea1b16ddfc22ff27e3f895f71ed4
Yogi Prakoso <yogiarrazy@gmail.com>
2024-06-21 19:05:05 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,10 @@ Author: GrayGrids
             var logo = document.querySelector('.navbar-brand img')
             if (window.pageYOffset > sticky) {
                 header_navbar.classList.add("sticky");
-                logo.src = '../../assets/images/logo/logo.svg';
+                logo.src = '../../assets/images/logo/logo-1.svg';
             } else {
                 header_navbar.classList.remove("sticky");
-                logo.src = '../../assets/images/logo/white-logo.svg';
+                logo.src = '../../assets/images/logo/white-logo-1.svg';
             }
 
             // show or hide the back-top-top button
